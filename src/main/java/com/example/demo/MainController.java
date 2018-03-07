bce4b1dfc44be851585fdabc06f3f9d9753fa792
Brandon Hammiller <bdayton1985@gmail.com>
2018-03-07 10:58:27 -0500

    Moved the methods around

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -6,6 +6,7 @@ import com.example.demo.Models.DateQuerry;
 import com.example.demo.Repositories.AppRoleRepository;
 import com.example.demo.Repositories.AppUserRepository;
 import com.example.demo.Repositories.DateQuerryRepository;
+import com.example.demo.Services.DateMethodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -34,6 +35,9 @@ public class MainController {
 
     @Autowired
     DateQuerryRepository dateQuerryRepository;
+
+    @Autowired
+    DateMethodsService dateFacts;
 
     LocalDate userDate=null;
 
@@ -96,132 +100,14 @@ public class MainController {
             System.out.println(result.toString());
             return "inputpage";
         } else {
+            dateQuerryRepository.save(dateQuerry);
             /*AppUser appUser = appUserRepository.findAppUserByAppUsername(authentication.getName());*/
-            userDate = LocalDate.parse(dateQuerry.getInputDate(),dTF);
-            dateQuerry.setOutputDate(userDate.format(shortMonthFormat));
+           /* userDate = LocalDate.parse(dateQuerry.getInputDate(),dTF);
+            dateQuerry.setOutputDate(userDate.format(shortMonthFormat));*/
             /*dateQuerry.setAppUser(appUser);*/
-            DayOfWeek dow = userDate.getDayOfWeek();
-            switch (dow){
-
-                case MONDAY:  dateQuerry.setMaleName("Kojo");
-                    dateQuerry.setFemaleName("Adjoa");
-                    dateQuerry.setDayOfWeek("Monday");
-                    dateQuerry.setDayCharacterists("Calm,peaceful");
-                    break;
-                case TUESDAY:  dateQuerry.setMaleName("Kwabena");
-                    dateQuerry.setFemaleName("Abena");
-                    dateQuerry.setDayOfWeek("Tuesday");
-                    dateQuerry.setDayCharacterists("Warrior, fierce, compassionate");
-                    break;
-                case WEDNESDAY:  dateQuerry.setMaleName("Kweku");
-                    dateQuerry.setFemaleName("Akua");
-                    dateQuerry.setDayOfWeek("Wednesday");
-                    dateQuerry.setDayCharacterists("Advocate, controlling");
-                    break;
-                case THURSDAY:  dateQuerry.setMaleName("Yaw");
-                    dateQuerry.setFemaleName("Yaa");
-                    dateQuerry.setDayOfWeek("Thursday");
-                    dateQuerry.setDayCharacterists("Confrontational, aggressive");
-                    break;
-                case FRIDAY:  dateQuerry.setMaleName("Kofi");
-                    dateQuerry.setFemaleName("Afua");
-                    dateQuerry.setDayOfWeek("Friday");
-                    dateQuerry.setDayCharacterists("Adventurous, creative, innovative");
-                    break;
-                case SATURDAY:  dateQuerry.setMaleName("Kwame");
-                    dateQuerry.setFemaleName("Ama");
-                    dateQuerry.setDayOfWeek("Saturday");
-                    dateQuerry.setDayCharacterists("The ancient wise one.");
-                    break;
-                case SUNDAY:  dateQuerry.setMaleName("Kwesi");
-                    dateQuerry.setFemaleName("Akosua");
-                    dateQuerry.setDayOfWeek("Sunday");
-                    dateQuerry.setDayCharacterists("Born leader, guide, protector");
-                    break;
-
-            }
-            int yearNum =userDate.getYear();
-            switch (yearNum % 12) {
-                case 0:  dateQuerry.setChineseZodiac("Monkey");
-                    break;
-                case 1:  dateQuerry.setChineseZodiac("Rooster");
-                    break;
-                case 2:  dateQuerry.setChineseZodiac("Dog");
-                    break;
-                case 3:  dateQuerry.setChineseZodiac("Pig");
-                    break;
-                case 4:  dateQuerry.setChineseZodiac("Rat");
-                    break;
-                case 5:  dateQuerry.setChineseZodiac("Ox");
-                    break;
-                case 6:  dateQuerry.setChineseZodiac("Tiger");
-                    break;
-                case 7:  dateQuerry.setChineseZodiac("Rabbit");
-                    break;
-                case 8:  dateQuerry.setChineseZodiac("Dragon");
-                    break;
-                case 9: dateQuerry.setChineseZodiac("Snake");
-                    break;
-                case 10: dateQuerry.setChineseZodiac("Horse");
-                    break;
-                case 11: dateQuerry.setChineseZodiac("Sheep");
-                    break;
-            }
-            int month= userDate.getMonthValue();
-            int day=userDate.getDayOfMonth();
-            if      ((month == 12 && day >= 22 && day <= 31) || (month ==  1 && day >= 1 && day <= 19))
-                dateQuerry.setAstrologicalSigns("Capricorn");
-            else if ((month ==  1 && day >= 20 && day <= 31) || (month ==  2 && day >= 1 && day <= 17))
-                dateQuerry.setAstrologicalSigns("Aquarius");
-            else if ((month ==  2 && day >= 18 && day <= 29) || (month ==  3 && day >= 1 && day <= 19))
-                dateQuerry.setAstrologicalSigns("Pisces");
-            else if ((month ==  3 && day >= 20 && day <= 31) || (month ==  4 && day >= 1 && day <= 19))
-                dateQuerry.setAstrologicalSigns("Aries");
-            else if ((month ==  4 && day >= 20 && day <= 30) || (month ==  5 && day >= 1 && day <= 20))
-                dateQuerry.setAstrologicalSigns("Taurus");
-            else if ((month ==  5 && day >= 21 && day <= 31) || (month ==  6 && day >= 1 && day <= 20))
-                dateQuerry.setAstrologicalSigns("Gemini");
-            else if ((month ==  6 && day >= 21 && day <= 30) || (month ==  7 && day >= 1 && day <= 22))
-                dateQuerry.setAstrologicalSigns("Cancer");
-            else if ((month ==  7 && day >= 23 && day <= 31) || (month ==  8 && day >= 1 && day <= 22))
-                dateQuerry.setAstrologicalSigns("Leo");
-            else if ((month ==  8 && day >= 23 && day <= 31) || (month ==  9 && day >= 1 && day <= 22))
-                dateQuerry.setAstrologicalSigns("Virgo");
-            else if ((month ==  9 && day >= 23 && day <= 30) || (month == 10 && day >= 1 && day <= 22))
-                dateQuerry.setAstrologicalSigns("Libra");
-            else if ((month == 10 && day >= 23 && day <= 31) || (month == 11 && day >= 1 && day <= 21))
-                dateQuerry.setAstrologicalSigns("Scorpio");
-            else if ((month == 11 && day >= 22 && day <= 30) || (month == 12 && day >= 1 && day <= 21))
-                dateQuerry.setAstrologicalSigns("Sagittarius");
-
-
-
-            /*switch (month) {
-                case 1:  dateQuerry.setAstrologicalSigns("Aries");
-                    break;
-                case 2:  dateQuerry.setAstrologicalSigns("Taurus");
-                    break;
-                case 3:  dateQuerry.setAstrologicalSigns("Gemini");
-                    break;
-                case 4:  dateQuerry.setAstrologicalSigns("Cancer");
-                    break;
-                case 5:  dateQuerry.setAstrologicalSigns("Leo");
-                    break;
-                case 6:  dateQuerry.setAstrologicalSigns("Virgo");
-                    break;
-                case 7:  dateQuerry.setAstrologicalSigns("Libra");
-                    break;
-                case 8:  dateQuerry.setAstrologicalSigns("Scorpio");
-                    break;
-                case 9:  dateQuerry.setAstrologicalSigns("Sagittarius");
-                    break;
-                case 10: dateQuerry.setAstrologicalSigns("Capricorn");
-                    break;
-                case 11: dateQuerry.setAstrologicalSigns("Aquarius");
-                    break;
-                case 12: dateQuerry.setAstrologicalSigns("Pisces");
-                    break;
-            }*/
+            dateFacts.addChineseZodiacYears(dateQuerry);
+            dateFacts.addDayCharacter(dateQuerry);
+            dateFacts.addWesternZodiac(dateQuerry);
             dateQuerryRepository.save(dateQuerry);
             /*appUser.addDateQuerry(dateQuerry);
             appUserRepository.save(appUser);*/
